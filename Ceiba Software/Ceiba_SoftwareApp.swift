92e315a961d7c264d42099474c4f660c958f363b
Pedro Carrillo <pf.cr@icloud.com>
2022-12-03 16:20:37 -0500

    Initial Commit

---
+++ Ceiba Software/Ceiba_SoftwareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ceiba_SoftwareApp.swift
+//  Ceiba Software
+//
+//  Created by Admin on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Ceiba_SoftwareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
